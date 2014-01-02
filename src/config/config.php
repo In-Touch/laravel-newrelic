80e4aadd4cc6cd800864518e4cb74e827f9812e6
Dan Wall <dan@mondodev.net>
2014-01-02 13:10:06 +1000

    Update automatic transaction naming - closes #6
    A few fixes for how updates to Laravel 4.1 affected transaction naming
    as well as support for custom name providers via a closure callback.
    
    More details: https://github.com/In-Touch/laravel-newrelic/issues/6

--- src/config/config.php
+++ src/config/config.php
@@ -10,9 +10,20 @@ return array(
 
     /*
      * Will automatically name transactions in NewRelic,
-     * using the Laravel route name
+     * using the Laravel route name, action or request
      */
     'auto_name_transactions' => true,
+
+    /*
+     * Define the name provider used when automatically naming transactions.
+     * Accepts either a closure or null to use the package default.
+     */
+    'name_provider' => null,
+
+    // 'name_provider' => function ($request, $response, $app) {
+    //     return $app['router']->currentRouteAction()
+    //         ?: $request->getMethod() . ' ' . $request->getPathInfo();
+    // },
 
     /*
      * Will cause an exception to be thrown if the NewRelic
