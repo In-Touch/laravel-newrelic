89a8de31857c84a983ca647cc5cf348622d922d4
Dan Hunsaker <danhunsaker@gmail.com>
2017-03-02 06:36:36 -0700

    Add support for Queued Job transactions
    Queued jobs should also have intelligent naming, and be marked properly as background tasks.

--- src/config/config.php
+++ src/config/config.php
@@ -18,6 +18,15 @@ return array(
 	'auto_name_transactions' => env('NEWRELIC_AUTO_NAME_TRANSACTION', true),
 
 	/*
+	 * Will automatically name queued jobs in NewRelic,
+	 * using the Laravel job class, data, or connection name.
+	 *
+	 * Set this to false to use the NewRelic default naming
+	 * scheme, or to set your own in your application.
+	 */
+	'auto_name_jobs' => env('NEWRELIC_AUTO_NAME_JOB', true),
+
+	/*
 	 * Define the name used when automatically naming transactions.
 	 * a token string:
 	 *      a pattern you define yourself, available tokens:
@@ -32,6 +41,23 @@ return array(
 	 *          'hello /world you really GET me'
 	 */
 	'name_provider' => env('NEWRELIC_NAME_PROVIDER', '{uri} {route}'),
+
+	/*
+	 * Define the name used when automatically naming queued jobs.
+	 * a token string:
+	 *      a pattern you define yourself, available tokens:
+	 *          {connection} = The name of the queue connection
+	 *          {class} = The name of the job class
+	 *          {data} = JSON string with all data passed to the job
+	 *          {args} = List of variable names passed to the job
+	 *          {input} = List of values passed to the job
+	 *      anything that is not a matched token will remain a string literal
+	 *      example:
+	 *          Given a job named App\MyJob, with data {"subject":"hello","to":"world"},
+	 *			the pattern 'I say {input} when I run {class}' would return:
+	 *			'I say hello, world when I run App\MyJob'
+	 */
+	'job_name_provider' => env('NEWRELIC_JOB_NAME_PROVIDER', '{class}'),
 
 	/*
 	 * Will cause an exception to be thrown if the NewRelic
